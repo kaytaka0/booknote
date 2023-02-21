d7573409d561d5e8e06b318626018fabc015d617
k-takashima <sora3takashima@gmail.com>
2023-02-21 19:29:01 +0900

    add  dotenv

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
-TARGET_FOLDER_NAME = 'ReadingList'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+TARGET_FOLDER_NAME = os.getenv('TARGET_FOLDER_NAME')
+USERNAME = os.getenv('USERNAME')
+PORT = os.getenv('PORT')
+SERVER_NAME = os.getenv('SERVER_NAME')
+FROM_ADDR = os.getenv('FROM_ADDR')
+TO_ADDR = os.getenv('TO_ADDR')
+CONNECTION_SECURITY = os.getenv('CONNECTION_SECURITY', 'STARTTLS')
+PASSWORD = os.getenv('PASSWORD')
